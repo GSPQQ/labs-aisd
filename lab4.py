232c4753c05e0f3923208c0c9eeb3780cc2a0b1a
GSPQQ <dannavolwo@gmail.com>
2025-05-08 14:17:39 +0400

    lab4.py

--- lab4.py
+++ lab4.py
@@ -1,65 +1,61 @@
-import numpy 
+import numpy as np
 import matplotlib.pyplot as plt
-def reader(filename):# Функция для чтения матрицы из файла
+
+def reader(filename):  
     with open(filename, 'r') as file:
         return [list(map(int, line.split())) for line in file]
-k = int(input("Введите k: "))# Ввод параметров
+# Ввод параметров
+k = int(input("Введите k: "))
 n = int(input("Введите n: "))
-A=reader('matrix2.txt') 
-A=numpy.copy(A)
-print("Матрица А:\n", A)
-F = numpy.copy(A)# Создаем копию матрицы A для F
-count_polozh, count_otric = 0, 0
-if n % 2 == 0: # Определяем размер подматриц
-    r = n // 2
+A = np.array(reader('matrix2.txt'))[:n, :n]  
+print("Матрица A:\n", A)
+
+F = np.copy(A)  
+half = n // 2
+E = F[half:, :half] if n % 2 == 0 else F[half + 1:, :half + 1]
+
+count_polozh = np.sum((E > 0) & (np.arange(E.shape[1]) % 2 == 1))  
+count_otric = np.sum((E < 0) & (np.arange(E.shape[1]) % 2 == 0))   
+
+if count_polozh > count_otric:
+    if n % 2 == 0:
+        B = F[half:, half:].copy()
+        C = F[:half, half:].copy()
+        F[:half, half:] = np.fliplr(B)
+        F[half:, half:] = np.fliplr(C)
+    else:
+        B = F[half + 1:, half + 1:].copy()
+        C = F[:half, half + 1:].copy()
+        F[:half, half + 1:] = np.fliplr(B)
+        F[half + 1:, half + 1:] = np.fliplr(C)
 else:
-    r = (n - 1) // 2 + 1
-for i in range(r, n):# Подсчет положительных и отрицательных элементов
-    for j in range(r if n % 2 == 0 else r - 1):
-        if F[i][j] > 0 and (j + 1) % 2 == 0:
-            count_polozh += 1
-        if F[i][j] < 0 and (j + 1) % 2 != 0:
-            count_otric += 1
-# Преобразование матрицы F по условию
-if count_polozh > count_otric: # Симметричный обмен
-    for i in range(r, n):
-        x = n - 1
-        for j in range(r if n % 2 == 0 else r - 1):
-            F[i][j], F[i][x] = F[i][x], F[i][j]
-            x -= 1
-else: # Несимметричный обмен
-    for i in range(r, n):
-        for j in range(r if n % 2 == 0 else r - 1):
-            F[i][j], F[i - r][j + r] = F[i - r][j + r], F[i][j]
+    if n % 2 == 0:
+        E = F[half:, :half].copy()
+        B = F[half:, half:].copy()
+        F[half:, :half] = B
+        F[half:, half:] = E
+    else:
+        E = F[half + 1:, :half + 1].copy()
+        B = F[half + 1:, half + 1:].copy()
+        F[half + 1:, :half + 1] = B
+        F[half + 1:, half + 1:] = E
 
 print("Матрица F:\n", F)
-
 try: # Вычисление необходимых матриц
-    C = numpy.linalg.inv(A)
-    G = numpy.tril(A)
-except numpy.linalg.LinAlgError:
+    A_inv = np.linalg.inv(A)
+    G = np.tril(A)
+except np.linalg.LinAlgError:
     print("Матрица A вырождена, невозможно вычислить обратную матрицу")
     exit()
-
-if round(numpy.linalg.det(A)) > numpy.trace(F):# Вычисление результата по условию
-    VIR = numpy.subtract(numpy.dot(A, A.T), k * numpy.dot(F, C))
-    print("\nТранспонированная матрица A:\n", A.T)
-    print("\nМатрица, обратная A:\n", C)
-    print("\nМатрица A, умноженная на свою транспонированную версию:\n", numpy.dot(A, A.T))
-    print("\nМатрица F, умноженная на матрицу, обратную A:\n", numpy.dot(F, C))
-    print("\nМатрица k*F*invA:\n", k * numpy.dot(F, C))
-    print("\nРезультат вычислений:\n", VIR)
+    # Вычисление результата по условию
+if round(np.linalg.det(A)) > np.trace(F):
+    VIR = np.subtract(np.dot(A, A.T), k * np.dot(F, A_inv))
+    print("\nРезультат вычислений (A*Aᵀ - k*F*A⁻¹):\n", VIR)
 else:
-    VIR = k * (numpy.subtract(numpy.add(k * C, G), F.T))
-    print("\nТранспонированная матрица F:\n", F.T)
-    print("\nНижняя треугольная матрица G:\n", G)
-    print("\nМатрица, обратная A:\n", C)
-    print("\nМатрица k*invA:\n", k * C)
-    print("\nМатрица k*invA+G:\n", numpy.add(k * C, G))
-    print("\nМатрица k*invA+G-F.T:\n", numpy.subtract(numpy.add(k * C, G), F.T))
-    print("\nРезультат вычислений:\n", VIR)
-
-plt.figure(figsize=(15, 5))# Построение графиков
+    VIR = k * (np.subtract(np.add(A_inv, G), F.T))
+    print("\nРезультат вычислений (k*(A⁻¹ + G - Fᵀ)):\n", VIR)
+# Визуализация
+plt.figure(figsize=(15, 5))
 # 1. Тепловая карта матрицы F
 plt.subplot(131)
 plt.imshow(F, cmap='winter', interpolation='nearest')
